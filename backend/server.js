a9f2bc972c84fb792193614d5106bb2926080544
Anshu Mittal <anshumittal200421@gmail.com>
2025-04-26 01:24:47 +0530

    add signup endpoint and store data

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // parse Json request bodies.
 
-app.use("/api/V1/auth", authRoutes)
+app.use("/api/v1/auth", authRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);

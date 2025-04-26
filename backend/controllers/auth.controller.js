48a448cd9ddef4e6942498814ee79236eecedecd
Anshu Mittal <anshumittal200421@gmail.com>
2025-04-27 02:00:17 +0530

    add getCurrentUser function endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -107,3 +107,12 @@ export const logout = (req, res) =>{
     res.clearCookie("jwt-linkedin");
     res.json({message: "logged out successfully."});
 }
+
+export const getCurrentUser = async (req, res)=>{
+    try{
+        res.json(req.user);
+    } catch(error){
+        console.error("Error in getCurrentUser controller: ", error);
+        res.status(500).json({message: "server error"});
+    }
+}
